14e9f14cd3f33ae73a9d830f9588e33300eb7a56
limhyoseok <nonamedoud@gmail.com>
2022-04-07 16:32:18 +0900

    [chore] upload alt_pic_nonblindApp

---
+++ alt-pic-nonblind/alt_pic_nonblindApp.swift
@@ -0,0 +1,17 @@
+//
+//  alt_pic_nonblindApp.swift
+//  alt-pic-nonblind
+//
+//  Created by hyo on 2022/04/05.
+//
+
+import SwiftUI
+
+@main
+struct alt_pic_nonblindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
