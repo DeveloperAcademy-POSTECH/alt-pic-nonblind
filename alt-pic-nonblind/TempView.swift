a69b5967d7f8e1f3ff9cb7416c9b74f7c3b33718
limhyoseok <nonamedoud@gmail.com>
2022-04-07 11:57:23 +0900

    [chore] upload tempView

---
+++ alt-pic-nonblind/TempView.swift
@@ -0,0 +1,32 @@
+//
+//  tempView.swift
+//  alt-pic-nonblind
+//
+//  Created by hyo on 2022/04/07.
+//
+
+import SwiftUI
+
+struct TempView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfileView: View{
+    var body: some View {
+        Text("프로필 뷰")
+    }
+}
+
+struct AltListView: View{
+    var body: some View {
+        Text("대체택스트 댓글 뷰")
+    }
+}
+
+struct tempView_Previews: PreviewProvider {
+    static var previews: some View {
+        TempView()
+    }
+}
