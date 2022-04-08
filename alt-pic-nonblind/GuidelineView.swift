7d7638f4ed1b1ad0df1d257b011397a6426b4bb4
monic98 <103012323+monic98@users.noreply.github.com>
2022-04-08 10:39:13 +0900

    [add] welcome file

---
+++ alt-pic-nonblind/GuidelineView.swift
@@ -0,0 +1,46 @@
+//
+//  Guideline 1.swift
+//  alt-pic-nonblind
+//
+//  Created by 부재원 on 2022/04/07.
+//
+
+import SwiftUI
+
+struct GuidelineView: View {
+    var body: some View {
+        ScrollView {
+           HStack {
+                PageView()
+            }
+        }
+    }
+}
+        
+struct Guideline_1_Previews: PreviewProvider {
+    static var previews: some View {
+        GuidelineView()
+    }
+}
+
+struct PageView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+                .frame(height: 80)
+            
+            TabView {
+                ForEach(0..<30) { i in
+                    ZStack {
+                        Image("가이드라인1")
+                            .resizable()
+                            .frame(width:390,height:390)
+                                        }.clipShape(RoundedRectangle(cornerRadius: 0.0, style: .continuous))
+                                    }
+                                    .padding(.all, 10)
+                                }
+                                .frame(width: UIScreen.main.bounds.width, height: 400)
+                            .tabViewStyle(PageTabViewStyle())
+        }
+                        }
+                    }
---
+++ alt-pic-nonblind/Welcome.swift
@@ -0,0 +1,56 @@
+//
+//  ContentView.swift
+//  alt-pic-nonblind
+//
+//  Created by hyo on 2022/04/05.
+//
+
+import SwiftUI
+
+struct Welcome: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text("듣는사진 도우미가 되신 것을 축하합니다!")
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .padding(.bottom, 60.0)
+                Image("아이콘")
+                    .resizable()
+                    .padding(.bottom)
+                    .frame(width: 190,height: 200)
+                Text("듣는사진은\n시각장애인을 위한\n대체텍스트 작성 공간이에요.\n어떻게 도와드릴 수 있는지\n간단히 살펴보고 시작할까요?")
+                    .font(.title3)
+                    .fontWeight(.light)
+                    .multilineTextAlignment(.center)
+                    .padding(.vertical, 40.0)
+                    .lineSpacing(10)
+                
+                ZStack {
+                    RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/10.0/*@END_MENU_TOKEN@*/)
+                        .frame(width:40, height:40)
+                        .padding(.bottom, 70.0)
+                    NavigationLink(destination: GuidelineView()) { Text("Go")
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 70.0)
+                    }
+                }
+                
+                
+                }
+
+        }
+        
+}
+    }
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Welcome()
+    }
+}
+//hello swift
+
