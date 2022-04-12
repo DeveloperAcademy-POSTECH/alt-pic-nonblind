fcb31baf2408e45a591b8a4d978c5e07e5688fa0
limhyoseok <nonamedoud@gmail.com>
2022-04-12 17:37:04 +0900

    [chore] remove navigationLink

--- alt-pic-nonblind/main/MainCardView.swift
+++ alt-pic-nonblind/main/MainCardView.swift
@@ -13,17 +13,15 @@ struct MainCardView: View {
     var body: some View {
         VStack {
             SnapCarousel(index: $currentIndex, items: altImageSample) {altimage in
-                NavigationLink(destination: AltListView()){
-                    GeometryReader{proxy in
-                        let size = proxy.size
-                        
-                        VStack(alignment: .leading, spacing: 2) {
-                            HStack(spacing: 2) {
-                                Image(systemName: "bubble.left")
-                                Text("\(altimage.altNum)")
-                            }
-                            ImageView(altimage: altimage, frameWidth: size.width, frameHeight: 400, cornerSize: 12)
+                GeometryReader{proxy in
+                    let size = proxy.size
+                    
+                    VStack(alignment: .leading, spacing: 2) {
+                        HStack(spacing: 2) {
+                            Image(systemName: "bubble.left")
+                            Text("\(altimage.altNum)")
                         }
+                        ImageView(altimage: altimage, frameWidth: size.width, frameHeight: 400, cornerSize: 12)
                     }
                 }
             }
