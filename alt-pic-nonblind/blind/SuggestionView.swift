0ec886e1546ee1f73ecd22585520fc86f2da572d
Hardy <dusgh15378@gmail.com>
2022-04-14 09:52:55 +0900

    [chore] fiz navigation link bug & ADD voiceover korean

--- alt-pic-nonblind/blind/SuggestionView.swift
+++ alt-pic-nonblind/blind/SuggestionView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct SuggestionView: View {
+    @State var navigatevalue : Bool = false
     @FocusState var isInputActive : Bool
     @Binding var SuggestionValue : blind_Values
     @Binding var sug_rootisActive : Bool
@@ -36,20 +37,33 @@ struct SuggestionView: View {
                 
                 }            }
             Spacer().frame(height:70)
-            //접수 완료 창으로 넘어감과 동시에 텍스트 에디터의 값이 배열로 삽입
-            NavigationLink( destination: CompleteView(completeValue: $SuggestionValue, PoptoRootView: self.$sug_rootisActive).navigationBarHidden( false).navigationBarBackButtonHidden(false))
-            {
-                Text("접수하기").font(.system(size:35)).foregroundColor(.white).padding(EdgeInsets(top: 20, leading: 50, bottom: 20, trailing: 50)).background(.black).cornerRadius(8).simultaneousGesture(TapGesture().onEnded{
+            //네비게이션 링크의 올바른 작성법?
+            NavigationLink {
+                CompleteView(completeValue: $SuggestionValue, PoptoRootView: self.$sug_rootisActive).simultaneousGesture(TapGesture().onEnded({ () in
                     SuggestionValue.arr_sug_values.append(SuggestionValue.sug_value)
                     print(SuggestionValue.arr_sug_values[0])
-                })
-
+                }))
+            } label: {
+                Text("접수하기").font(.system(size:35)).foregroundColor(.white).padding(EdgeInsets(top: 20, leading: 50, bottom: 20, trailing: 50)).background(.black).cornerRadius(8)
+                    
             }.isDetailLink(false)
+
+
             
             Spacer().frame(height:50)
         }
     }
+
 }
+//접수 완료 창으로 넘어감과 동시에 텍스트 에디터의 값이 배열로 삽입
+//            NavigationLink( destination:CompleteView(completeValue: $SuggestionValue, PoptoRootView: self.$sug_rootisActive))
+//            {
+//                Text("접수하기").font(.system(size:35)).foregroundColor(.white).padding(EdgeInsets(top: 20, leading: 50, bottom: 20, trailing: 50)).background(.black).cornerRadius(8).simultaneousGesture(TapGesture().onEnded{
+//                    SuggestionValue.arr_sug_values.append(SuggestionValue.sug_value)
+//                    print("Sug_working")
+//                })
+//
+//            }.isDetailLink(false)
 
 //struct SuggestionView_Previews: PreviewProvider {
 //    static var previews: some View {
